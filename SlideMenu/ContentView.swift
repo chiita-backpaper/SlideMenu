84adf2cee0c38c73b13439f9c02a7b5beb4a8d04
Taichi Uragami <tu.0514.0315@icloud.com>
2021-11-01 13:08:33 +0900

    Initial Commit

---
+++ SlideMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlideMenu
+//
+//  Created by Taichi Uragami on 2021/11/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlideMenu/SlideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMenuApp.swift
+//  SlideMenu
+//
+//  Created by Taichi Uragami on 2021/11/01.
+//
+
+import SwiftUI
+
+@main
+struct SlideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
